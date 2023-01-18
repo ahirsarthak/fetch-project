e55fffaf83a07ad3c18e77cec27ba38bbc4439a6
ahirsarthak <sarthakahir@gmail.com>
2023-01-17 21:40:50 -0500

    Final_Update

--- task.py
+++ task.py
@@ -49,28 +49,39 @@ def write_to_db(data):
 
 
 while True:
+    try:
     
-    # Receive message from SQS queue
-    response = subprocess.run(['awslocal', 'sqs', 'receive-message', '--queue-url',
+        # Receive message from SQS queue
+        response = subprocess.run(['awslocal', 'sqs', 'receive-message', '--queue-url',
                                queue_url], capture_output=True)
-    if response.returncode == 0:
-        # Extract the message body
-        message_body = json.loads(response.stdout)
-    # Extract the user_id field
-        receipt_handle = message_body['Messages'][0]['ReceiptHandle']
+        if response.returncode == 0:
+            # Extract the message body
+            message_body = json.loads(response.stdout)
+        # Extract the user_id field
+            receipt_handle = message_body['Messages'][0]['ReceiptHandle']
 
-        data = json.loads(message_body['Messages'][0]['Body'])
-        print(data)
+            data = json.loads(message_body['Messages'][0]['Body'])
+            print(data)
 
-        subprocess.run(['awslocal', 'sqs', 'delete-message', '--queue-url',
-                        queue_url, '--receipt-handle', receipt_handle], capture_output=True)
+            subprocess.run(['awslocal', 'sqs', 'delete-message', '--queue-url',
+                       queue_url, '--receipt-handle', receipt_handle], capture_output=True)
+            
+            # IF there is invalid / unwanted message in Queue to ignore and remove it
+            if data.get('user_id') == None:
+                continue
+            
+            data = mask_pii(data)
+            data["app_version"] = str(data["app_version"])
+            write_to_db(data)
+            # print(data)
+            print("Process End")
 
-        data = mask_pii(data)
-        data["app_version"] = str(data["app_version"])
-        write_to_db(data)
-        # print(data)
-        print("Process End")
+        else:
+            print(response.stderr)
+        # time.sleep(5)
+    except Exception as e:
+        print(e)
 
-    else:
-        print(response.stderr)
-    # time.sleep(5)
+    finally:
+        print("ETL Process Complete")
+        break
